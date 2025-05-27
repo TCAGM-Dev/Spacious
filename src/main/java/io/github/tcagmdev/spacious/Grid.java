9dfc8afa6400cda336de0ea1bee9cdc313ecbe8e
TCAGM-Dev <thomas@andringa.org>
2025-05-27 11:29:23 +0200

    Initial commit

---
+++ src/main/java/io/github/tcagmdev/spacious/Grid.java
@@ -0,0 +1,190 @@
+package io.github.tcagmdev.spacious;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class Grid<T> {
+	private final FixedSizeList<FixedSizeList<T>> grid;
+
+	public Grid(int width, int height) {
+		this.grid = new FixedSizeList<>(height);
+		for (int i = 0; i < this.grid.size(); i++) this.grid.set(i, new FixedSizeList<>(width));
+	}
+	public Grid(Vec2i size) {
+		this(size.getX(), size.getY());
+	}
+	public Grid(Grid<T> grid) {
+		this.grid = new FixedSizeList<>(grid.getHeight());
+		for (int y = 0; y < grid.getHeight(); y++) this.grid.set(y, new FixedSizeList<>(grid.grid.get(y)));
+	}
+	public Grid(T[][] grid) {
+		this.grid = new FixedSizeList<>(grid.length);
+		for (int i = 0; i < grid.length; i++) this.grid.set(i, new FixedSizeList<>(grid[i]));
+	}
+
+	public T set(int x, int y, T value) {
+		if (!this.isInBounds(x, y)) throw new IndexOutOfBoundsException(String.format("Position (%d, %d) out of bounds for size (%d, %d)", x, y, this.getWidth(), this.getHeight()));
+		this.grid.get(y).set(x, value);
+		return value;
+	}
+	public T set(Vec2i position, T value) {
+		return this.set(position.getX(), position.getY(), value);
+	}
+	public void paste(int x, int y, Grid<T> grid) {
+		this.paste(new Vec2i(x, y), grid);
+	}
+	public void paste(Vec2i position, Grid<T> grid) {
+		if (!this.isInBounds(position) || !this.isInBounds(position.add(grid.size().subtract(Vec2i.IDENTITY)))) throw new IndexOutOfBoundsException("Pasting grid is out of bounds");
+
+		grid.forEach((v, offset) -> this.set(position.add(offset), v));
+	}
+	public T get(int x, int y) {
+		if (!this.isInBounds(x, y)) throw new IndexOutOfBoundsException(String.format("Position (%d, %d) out of bounds for size (%d, %d)", x, y, this.getWidth(), this.getHeight()));
+		return this.grid.get(y).get(x);
+	}
+	public T get(Vec2i position) {
+		return this.get(position.getX(), position.getY());
+	}
+
+	public boolean exists(int x, int y) {
+		return this.isInBounds(x, y) && this.get(x, y) != null;
+	}
+	public boolean exists(Vec2i position) {
+		return this.exists(position.getX(), position.getY());
+	}
+
+	public Vec2i find(Predicate<T> predicate) {
+		for (int x = 0; x < this.getWidth(); x++) for (int y = 0; y < this.getHeight(); y++) {
+			if (predicate.test(this.get(x, y))) return new Vec2i(x, y);
+		}
+		return null;
+	}
+	public Vec2i find(T value) {
+		return this.find(v -> v.equals(value));
+	}
+
+	@FunctionalInterface
+	public interface ForEachCallback<R> {
+		void handle(R value, Vec2i position);
+	}
+	public void forEach(ForEachCallback<T> callback) {
+		for (int y = 0; y < this.getHeight(); y++) for (int x = 0; x < this.getWidth(); x++) {
+			callback.handle(this.get(x, y), new Vec2i(x, y));
+		}
+	}
+
+	public List<Vec2i> positions() {
+		List<Vec2i> result = new ArrayList<>();
+
+		this.forEach((value, position) -> result.add(position));
+
+		return result;
+	}
+	public List<T> values() {
+		List<T> result = new ArrayList<>();
+
+		this.forEach((value, position) -> result.add(value));
+
+		return result;
+	}
+
+	public Grid<T> fill(Function<Vec2i, T> provider) {
+		this.forEach((value, position) -> this.set(position, provider.apply(position)));
+		return this;
+	}
+	public Grid<T> fill(T value) {
+		return this.fill(position -> value);
+	}
+
+	public int getHeight() {
+		return this.grid.size();
+	}
+	public int getWidth() {
+		return this.grid.getFirst().size();
+	}
+	public Vec2i size() {
+		return new Vec2i(this.getWidth(), this.getHeight());
+	}
+	public int getElementCount() {
+		return this.getWidth() * this.getHeight();
+	}
+
+	public boolean isInBounds(int x, int y) {
+		return x >= 0 && x < this.getWidth() && y >= 0 && y < this.getHeight();
+	}
+	public boolean isInBounds(Vec2i position) {
+		return this.isInBounds(position.getX(), position.getY());
+	}
+
+	public FixedSizeList<T> getRow(int y) {
+		return new io.github.tcagmdev.spacious.FixedSizeList<>(this.grid.get(y));
+	}
+	public FixedSizeList<T> getFirstRow() {
+		return this.getRow(0);
+	}
+	public FixedSizeList<T> getLastRow() {
+		return this.getRow(this.getHeight() - 1);
+	}
+	public FixedSizeList<T> getColumn(int x) {
+		FixedSizeList<T> result = new FixedSizeList<>(this.getHeight());
+
+		for (int y = 0; y < result.size(); y++) {
+			result.set(y, this.grid.get(y).get(x));
+		}
+
+		return result;
+	}
+	public FixedSizeList<T> getFirstColumn() {
+		return this.getColumn(0);
+	}
+	public FixedSizeList<T> getLastColumn() {
+		return this.getColumn(this.getWidth() - 1);
+	}
+
+	public FixedSizeList<FixedSizeList<T>> getRows() {
+		return this.grid;
+	}
+
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		for (int y = 0; y < this.getHeight(); y++) {
+			for (int x = 0; x < this.getWidth(); x++) {
+				builder.append(String.valueOf(this.get(x, y)));
+			}
+			builder.append('\n');
+		}
+		return builder.toString();
+	}
+
+	@FunctionalInterface
+	public interface FullMapFunction<R, K> {
+		K apply(R value, Vec2i position);
+	}
+	public <K> Grid<K> map(FullMapFunction<T, K> mapFunction) {
+		Grid<K> result = new Grid<>(this.getWidth(), this.getHeight());
+		for (int y = 0; y < this.getHeight(); y++) for (int x = 0; x < this.getWidth(); x++) {
+			result.set(x, y, mapFunction.apply(this.get(x, y), new Vec2i(x, y)));
+		}
+		return result;
+	}
+
+	public <K> Grid<K> map(Function<T, K> mapFunction) {
+		return this.map((value, _) -> mapFunction.apply(value));
+	}
+
+	public static Grid<Character> fromCharGrid(String charGrid) {
+		String[] lines = charGrid.split("\n");
+
+		int width = lines[0].length() - 1;
+		int height = lines.length;
+
+		return new Grid<Character>(width, height).fill(position -> lines[position.getY()].charAt(position.getX()));
+	}
+
+	public static <K> Grid<K> ofSingle(K value) {
+		Grid<K> grid = new Grid<>(1, 1);
+		grid.set(0, 0, value);
+		return grid;
+	}
+}
---
+++ src/main/java/io/github/tcagmdev/spacious/Vec2f.java
@@ -0,0 +1,68 @@
+package io.github.tcagmdev.spacious;
+
+public class Vec2f {
+	private final float x;
+	private final float y;
+
+	public Vec2f(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public float getX() {
+		return this.x;
+	}
+	public float getY() {
+		return this.y;
+	}
+
+	public Vec2f invert() {
+		return new Vec2f(-this.x, -this.y);
+	}
+
+	public Vec2f add(Vec2f other) {
+		return new Vec2f(this.x + other.x, this.y + other.y);
+	}
+	public Vec2f subtract(Vec2f other) {
+		return new Vec2f(this.x - other.x, this.y - other.y);
+	}
+	public Vec2f add(Vec2i other) {
+		return new Vec2f(this.x + other.getX(), this.y + other.getY());
+	}
+	public Vec2f subtract(Vec2i other) {
+		return new Vec2f(this.x - other.getX(), this.y - other.getY());
+	}
+
+	public Vec2f multiply(float factor) {
+		return new Vec2f(this.x * factor, this.y * factor);
+	}
+	public Vec2f divide(float divisor) {
+		return new Vec2f(this.x / divisor, this.y / divisor);
+	}
+
+	public Vec2f modulo(Vec2f divisor) {
+		return new Vec2f(this.x % divisor.x, this.y % divisor.y);
+	}
+	public Vec2i round() {
+		return new Vec2i(Math.round(this.x), Math.round(this.y));
+	}
+
+	public int squaredMagnitude() {
+		return (int) (Math.pow(this.x, 2) + Math.pow(this.y, 2));
+	}
+	public double magnitude() {
+		return Math.sqrt(this.squaredMagnitude());
+	}
+
+	public boolean equals(Vec2f other) {
+		return this.x == other.x && this.y == other.y;
+	}
+	public String toString() {
+		return String.format("(%f, %f)", this.x, this.y);
+	}
+
+	public static final Vec2f ZERO = new Vec2f(0, 0);
+	public static final Vec2f X_AXIS = new Vec2f(1, 0);
+	public static final Vec2f Y_AXIS = new Vec2f(0, 1);
+	public static final Vec2f IDENTITY = X_AXIS.add(Y_AXIS);
+}
